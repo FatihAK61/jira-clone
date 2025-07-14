a94d0691a55917edf68eee859f0018538c86a62b
FatihAK61 <fatihak61@gmail.com>
2025-07-14 19:22:27 +0300

    new test

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,12 +1,14 @@
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {DottedSeperator} from "@/components/custom/dotted-seperator";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 
 const SignInCard = () => {
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl font-bold">
-                    Welcome to the Sign In Page
+                    Welcome Back!
                 </CardTitle>
             </CardHeader>
             <div>
@@ -14,8 +16,25 @@ const SignInCard = () => {
             </div>
             <CardContent className="p-7">
                 <form className="space-y-4" action="#" method="POST">
-
+                    <Input required type="email" placeholder="Enter Email Address"
+                           value={""} onChange={() => {
+                    }} disabled={false}/>
+                    <Input required type="password" placeholder="Enter Password"
+                           value={""} onChange={() => {
+                    }} disabled={false} min={8} max={24}/>
+                    <Button disabled={false} size="lg" className="w-full">Login</Button>
                 </form>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeperator/>
+            </div>
+            <CardContent className="p-7 flex flex-col gap-y-4">
+                <Button variant="secondary" size="lg" disabled={false} className="w-full">
+                    Login with Google
+                </Button>
+                <Button variant="secondary" size="lg" disabled={false} className="w-full">
+                    Login with Github
+                </Button>
             </CardContent>
         </Card>
     );
