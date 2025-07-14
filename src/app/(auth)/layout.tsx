82f6efc04740be95d78c57415305e0c225e9b16f
FatihAK61 <fatihak61@gmail.com>
2025-07-14 20:57:13 +0300

    new test

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,28 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {usePathname} from "next/navigation";
+import Link from "next/link";
 
 interface AuthLayoutProps {
     children: React.ReactNode;
 }
 
 const AuthLayout = ({children}: AuthLayoutProps) => {
+
+    const pathname = usePathname();
+    const isSignIn = pathname === "/sign-in";
+
     return (
         <main className="bg-neutral-100 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
                 <nav className="flex items-center justify-between">
-                    <Image src="/logo.svg" alt="logo" height={48} width={180}/>
-                    <Button variant="secondary">
-                        Sign Up
+                    <Image priority src="/logo.svg" alt="logo" height={38} width={153}/>
+                    <Button asChild variant="secondary">
+                        <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                            {isSignIn ? "Sign Up" : "Login"}
+                        </Link>
                     </Button>
                 </nav>
                 <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,24 +1,26 @@
 import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {DottedSeperator} from "@/components/custom/dotted-seperator";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {FcGoogle} from "react-icons/fc";
 import {FaGithub} from "react-icons/fa";
 import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
 
 const formSchema = z.object({
+    name: z.string().min(1, "Name is required").max(50, "Name must be at most 50 characters"),
     email: z.email("Invalid email address!"),
     password: z.string().min(8, "Password must be at least 8 characters").max(24, "Password must be at most 24 characters"),
 });
 
-const SignInCard = () => {
+const SignUpCard = () => {
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            name: "",
             email: "",
             password: ""
         },
@@ -33,8 +35,15 @@ const SignInCard = () => {
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl font-bold">
-                    Welcome Back!
+                    Sign Up
                 </CardTitle>
+                <CardDescription>
+                    Create an account to get started with our services.
+                    <br/>
+                    <br/>
+                    By signing up, you agree to our <a href="/terms" className="text-blue-500 hover:underline">Terms of
+                    Service</a> and <a href="/privacy" className="text-blue-500 hover:underline">Privacy Policy</a>.
+                </CardDescription>
             </CardHeader>
             <div>
                 <DottedSeperator/>
@@ -42,6 +51,17 @@ const SignInCard = () => {
             <CardContent className="p-7">
                 <Form {...form}>
                     <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            name="name"
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input {...field} type="text" placeholder="Enter Name"/>
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}/>
                         <FormField
                             name="email"
                             control={form.control}
@@ -64,7 +84,7 @@ const SignInCard = () => {
                                     <FormMessage/>
                                 </FormItem>
                             )}/>
-                        <Button disabled={false} size="lg" className="w-full">Login</Button>
+                        <Button disabled={false} size="lg" className="w-full">Sign Up</Button>
                     </form>
                 </Form>
             </CardContent>
@@ -74,11 +94,11 @@ const SignInCard = () => {
             <CardContent className="p-7 flex flex-col gap-y-4">
                 <Button variant="secondary" size="lg" disabled={false} className="w-full">
                     <FcGoogle className="mr-2 size-5"/>
-                    Login with Google
+                    SignUp with Google
                 </Button>
                 <Button variant="secondary" size="lg" disabled={false} className="w-full">
                     <FaGithub className="mr-2 size-5"/>
-                    Login with Github
+                    SignUp with Github
                 </Button>
             </CardContent>
             <div className="px-7">
@@ -86,11 +106,12 @@ const SignInCard = () => {
             </div>
             <CardContent className="p-7 flex items-center justify-center">
                 <p className="text-center text-sm text-muted-foreground">
-                    Don&apos;t have an account? <a href="/sign-up" className="text-blue-500 hover:underline">Sign Up</a>
+                    Already have an account? <a href="/sign-in" className="text-blue-500 hover:underline">Sign
+                    In</a>
                 </p>
             </CardContent>
         </Card>
     );
 }
 
-export default SignInCard;
+export default SignUpCard;
