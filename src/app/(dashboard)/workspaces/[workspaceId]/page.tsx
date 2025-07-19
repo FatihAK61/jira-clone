d8c6482f0456b076b4b649f0f00b60f201d9560d
FatihAK61 <fatihak61@gmail.com>
2025-07-19 12:14:57 +0300

    new test

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,9 @@
-const WorkspacePage = () => {
+import {getCurrent} from "@/features/auth/actions";
+import {redirect} from "next/navigation";
+
+const WorkspaceIdPage = async () => {
+    const user = await getCurrent();
+    if (!user) redirect("/sign-in")
     return (
         <div className="flex h-full w-full flex-col">
             <div className="flex h-full w-full flex-col">
@@ -11,4 +16,4 @@ const WorkspacePage = () => {
     );
 }
 
-export default WorkspacePage;
+export default WorkspaceIdPage;
