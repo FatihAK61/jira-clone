b074e20efa7cfce4add6bec961daa7e7af527802
FatihAK61 <fatihak61@gmail.com>
2025-07-28 22:33:02 +0300

    New Update

--- src/features/tasks/components/create-task-modal.tsx
+++ src/features/tasks/components/create-task-modal.tsx
@@ -1,16 +1,15 @@
 "use client";
 import {useCreateTaskModal} from "@/features/tasks/hooks/use-create-task-modal";
 import {ResponsiveModal} from "@/components/custom/responsive-modal";
+import {CreateTaskFormWrapper} from "@/features/tasks/components/create-task-form-wrapper";
 
 
 const CreateTaskModal = () => {
-    const {isOpen, setIsOpen} = useCreateTaskModal();
+    const {isOpen, setIsOpen, close} = useCreateTaskModal();
 
     return (
         <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
-            <div>
-                Todo: task form
-            </div>
+            <CreateTaskFormWrapper onCancel={close}/>
         </ResponsiveModal>
     );
 }
